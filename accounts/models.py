654511527a7366aef96f12cdbc4f1a33be7598f0
Shakilahmmedms <shakilahmmed@gmail.com>
2024-06-10 16:23:35 +0600

    image field fix

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
-    image = models.ImageField(upload_to='patient/images/')
+    image = models.ImageField(upload_to='accounts/images/')
     mobile_no = models.CharField(max_length = 12)
     
     def __str__(self):
